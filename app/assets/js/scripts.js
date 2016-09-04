fdc1c444e38ccce86134661df82a5afa5d3de5dc
a o <a o>
2016-09-04 12:40:38 -0400

    added display flex to footer fixed alignment in mobile layout

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
  * fastshell
  * Fiercely quick and opinionated front-ends
- * https://HosseinKarami.github.io/fastshell
+ * 
  * @author Hossein Karami
  * @version 1.0.5
  * Copyright 2016. MIT licensed.
